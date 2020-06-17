a24751b4c42b269e1626436beff786399c022deb
lewollerenshaw <lewollerenshaw@gmail.com>
2020-06-17 17:27:46 +0100

    Initial commit, expo project, color palette, linting

--- App.js
+++ App.js
@@ -1,36 +1,32 @@
 import * as React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
+import { Colors } from './styles/colors';
 
-const instructions = Platform.select({
-  ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
-  android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
+const { statusBarHeight } = Constants;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.$lightShade,
+    padding: 10,
+  },
+  body: {
+    paddingTop: statusBarHeight,
+    color: Colors.$info,
+  },
+  heading: {
+    fontSize: 32,
+  },
 });
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <Text style={styles.instructions}>To get started, edit App.js</Text>
-      <Text style={styles.instructions}>{instructions}</Text>
+      <View style={styles.body}>
+        <Text style={styles.heading}>Collection</Text>
+        <Text style={styles.text}> Body text, testing that it looks ok</Text>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
---
+++ styles/colors.js
@@ -0,0 +1,15 @@
+const Colors = {
+  $primary: '#1894e3',
+  $darkShade: '#2a3746',
+  $darkAccent: '#828385',
+  $lightShade: '#fbfbfa',
+  $lightAccent: '#91bcd4',
+  $info: '#293746',
+  $success: '#3ca77c',
+  $warning: '#ba9744',
+  $danger: '#f44336',
+};
+
+module.exports = {
+  Colors,
+};
