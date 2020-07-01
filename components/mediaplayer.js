61d44f8535b7c905edffaa385bbe761529eb40a7
calcmp <calcmp@gmail.com>
2020-07-01 17:41:55 +0100

    Implemented sharing functionality

--- components/mediaplayer.js
+++ components/mediaplayer.js
@@ -1,20 +1,28 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'red',
-  },
-});
+import {
+  View, Text, TouchableOpacity,
+} from 'react-native';
+import * as Sharing from 'expo-sharing';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faShare } from '@fortawesome/free-solid-svg-icons';
+import mediaplayerStyles from '../styles/mediaplayer';
 
 function mediaplayer({ open, rec }) {
+  const handleShare = async () => {
+    if (!(await Sharing.isAvailableAsync())) {
+      alert('Uh oh, sharing isn\'t available on your platform');
+      return;
+    }
+    Sharing.shareAsync(rec.URI);
+  };
+
   const height = open === true ? 'auto' : 0;
   return (
-    <View style={[styles.container, {
-      height,
-    }]}
-    >
-      <Text />
+    <View style={[mediaplayerStyles.container, { height }]}>
+      <TouchableOpacity onPress={handleShare}>
+        <FontAwesomeIcon icon={faShare} />
+      </TouchableOpacity>
+      <Text>{console.log(rec)}</Text>
     </View>
   );
 }
---
+++ styles/mediaplayer.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+import { Colors } from './colors';
+
+const mediaplayerStyles = StyleSheet.create({
+  container: {
+    backgroundColor: 'red',
+  },
+});
+
+export default mediaplayerStyles;
